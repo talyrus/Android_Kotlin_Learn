7e9e26ff7f5c6aa3ad7140af537c2eed1dfcf785
talyrus <talyrus@users.noreply.github.com>
2021-11-09 21:22:27 +0300

    add Toast to Recycler project

--- app/src/main/java/study/talyrus/myrecyclerview/UserViewHolder.kt
+++ app/src/main/java/study/talyrus/myrecyclerview/UserViewHolder.kt
@@ -2,6 +2,7 @@ package study.talyrus.myrecyclerview
 
 import android.view.View
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 
 class UserViewHolder(itemView: View) :
@@ -9,5 +10,9 @@ class UserViewHolder(itemView: View) :
     fun bind(userName: String) {
         val userNameTextView: TextView = itemView.findViewById(R.id.users_name_text_view)
         userNameTextView.text = userName
+
+        itemView.setOnClickListener{
+            Toast.makeText(itemView.context, userName, Toast.LENGTH_LONG).show()
+        }
     }
 }
